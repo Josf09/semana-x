83d45ec3344010883c338884202e1d4e8a5fb991
Josf09 <josealbertosalazarfernandez@gmail.com>
2026-09-24 16:47:41 +0000

    Actualiza pantalla principal

---
+++ mini-lms/src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+selector: 'app-root',
+templateUrl: './app.html'
+})
+export class App {
+titulo = 'Mini-LMS · Semana 7';
+nombre = 'Juan Carlos';
+}
